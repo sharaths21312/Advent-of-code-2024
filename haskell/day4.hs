b53b06878ededa8fc79e94dc91f2cb287b9004fc
sharaths21312 <62636355+sharaths21312@users.noreply.github.com>
2024-12-13 15:30:43 +0530

    haskell day 4

---
+++ haskell/day4.hs
@@ -0,0 +1,39 @@
+import Utils
+import qualified Data.Text as T
+import qualified Data.Text.IO as IO
+import qualified Data.Map.Strict as M
+import qualified Data.Vector as V
+import Text.Printf
+import Utils
+
+countxmas :: M.Map (Integer, Integer) Char -> [((Integer, Integer), Char)] -> Int
+countxmas _ [] = 0
+countxmas mp (((x, y), _):xs) = countxmas mp xs +
+        length (filter id [mc up, mc down, mc left, mc right, 
+        mc upleft, mc upright, mc downleft, mc downright])
+    where
+        up = [(x, y - diff) | diff <- [0..3]]
+        down = [(x, y + diff) | diff <- [0..3]]
+        left = [(x - diff, y) | diff <- [0..3]]
+        right = [(x + diff, y) | diff <- [0..3]]
+        upleft = [(x - diff, y - diff) | diff <- [0..3]]
+        upright = [(x + diff, y - diff) | diff <- [0..3]]
+        downleft = [(x - diff, y + diff) | diff <- [0..3]]
+        downright = [(x + diff, y + diff) | diff <- [0..3]]
+        mc = flip (matchListMap mp) "XMAS"
+
+countxmas2 :: M.Map (Integer, Integer) Char -> [((Integer, Integer), Char)] -> Int
+countxmas2 _ [] = 0
+countxmas2 mp (((x, y), _):xs) = countxmas2 mp xs +
+    fromEnum ((mc diag1 || mc' diag1) && (mc diag2 || mc' diag2))
+    where
+        diag1 = [(x - diff, y - diff) | diff <- [-1..1]]
+        diag2 = [(x + diff, y - diff) | diff <- [-1..1]]
+        mc = flip (matchListMap mp) "MAS"
+        mc' = flip (matchListMap mp) "SAM"
+
+main = do
+    filedata <- T.strip <$> IO.readFile "../inputs/day4.txt"
+    let c = textIntoCoords filedata
+    print $ countxmas c $ filter (\(_, c) -> c == 'X') $ M.toList c
+    print $ countxmas2 c $ filter (\(_, c) -> c == 'A') $ M.toList c
--- haskell/utils.hs
+++ haskell/utils.hs
@@ -1,8 +1,25 @@
 module Utils where
 import qualified Data.Text as T
+import qualified Data.Map.Strict as M
 
 readInt :: TextLike t => t -> Integer
 readInt = read . toString
+
+textIntoCoords :: TextLike t => t -> M.Map (Integer, Integer) Char
+textIntoCoords txt = M.fromList $ zip coords (concat $ lines str)
+    where
+        str = toString txt
+        lenx = length $ head $ lines str
+        leny = length $ lines str
+        coords = [(toInteger x, toInteger y) | x <- [0..lenx-1], y <- [0..leny-1]]
+
+matchListMap :: (Ord k, Eq a) => M.Map k a -> [k] -> [a] -> Bool
+matchListMap _ [] [] = True
+matchListMap mp (k:ks) (v:vs) =
+    case M.lookup k mp of
+    Just val -> (v == val) && matchListMap mp ks vs
+    Nothing -> False
+matchListMap _ _ _ = False -- This only runs if the lists are of unequal length
 
 class TextLike a where
     toText :: a -> T.Text
